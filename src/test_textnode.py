a796d0294ecfda7cf9efc4075901532f92804a98
jdra000 <juandavidrey@icloud.com>
2025-01-07 13:02:50 -0500

    working

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,17 @@
 import unittest
 
-from textnode import TextType, TextNode, text_node_to_html_node
+# textnode classes
+from textnode import TextType, TextNode
+
+# textnode functions
+from textnode import(
+	text_node_to_html_node, 
+	split_nodes_delimiter, extract_markdown_images, 
+	extract_markdown_links,
+	split_nodes_images,
+	split_nodes_links,
+	text_to_textnodes)
+
 from htmlnode import LeafNode
 
 class TestTextNode(unittest.TestCase):
@@ -18,7 +29,92 @@ class TestTextNode(unittest.TestCase):
 		node = TextNode("This is a text node", TextType.BOLD)
 		self.assertIsInstance(node.text_type, TextType)
 
-	# Test function
+	# split_nodes_delimiter function
+	def test_split_nodes_delimiter_bold(self):
+		node1 = TextNode("This is text with a **bold block** word", TextType.TEXT)
+		node2 = TextNode("This is text with a **bold sentence** phrase", TextType.TEXT)
+
+		new_nodes = split_nodes_delimiter([node1, node2], "**", TextType.BOLD)
+
+		expected_nodes = [
+  			TextNode("This is text with a ", TextType.TEXT),	
+  			TextNode("bold block", TextType.BOLD),
+            TextNode(" word", TextType.TEXT),
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("bold sentence", TextType.BOLD),
+            TextNode(" phrase", TextType.TEXT)
+        ]
+		self.assertEqual(new_nodes, expected_nodes)
+
+	# split_nodes_images function
+	def test_split_nodes_images(self):
+		node = TextNode("This is text with an image ![to boot dev](https://www.boot.dev) and ![to youtube](https://www.youtube.com/@bootdotdev)", TextType.TEXT)
+
+		new_nodes = split_nodes_images([node])
+
+		expected_nodes = [
+			TextNode("This is text with an image ", TextType.TEXT),
+			TextNode("to boot dev", TextType.IMAGE, "https://www.boot.dev"),
+			TextNode(" and ", TextType.TEXT),
+			TextNode("to youtube", TextType.IMAGE, "https://www.youtube.com/@bootdotdev")
+		]
+
+		self.assertEqual(new_nodes, expected_nodes)
+
+
+	def test_split_nodes_links(self):
+		node = TextNode("This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)", TextType.TEXT)
+
+		new_nodes = split_nodes_links([node])
+
+		expected_nodes = [
+			TextNode("This is text with a link ", TextType.TEXT),
+			TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+			TextNode(" and ", TextType.TEXT),
+			TextNode("to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev")
+		]
+
+		self.assertEqual(new_nodes, expected_nodes)
+
+
+	def test_text_to_textnodes(self):
+		text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+
+		new_nodes = text_to_textnodes(text)
+		
+		expected_nodes = [
+		    TextNode("This is ", TextType.TEXT),
+		    TextNode("text", TextType.BOLD),
+		    TextNode(" with an ", TextType.TEXT),
+		    TextNode("italic", TextType.ITALIC),
+		    TextNode(" word and a ", TextType.TEXT),
+		    TextNode("code block", TextType.CODE),
+		    TextNode(" and an ", TextType.TEXT),
+		    TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+		    TextNode(" and a ", TextType.TEXT),
+		    TextNode("link", TextType.LINK, "https://boot.dev"),
+		]
+		
+		self.assertEqual(new_nodes, expected_nodes)
+
+	# extract_markdown_images function
+	def test_extract_markdown_images(self):
+		text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+		result = extract_markdown_images(text)
+		expected_result = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+
+		self.assertEqual(result, expected_result)
+
+	# extract_markdown_links function
+	def test_extract_markdown_links(self):
+		text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+		result = extract_markdown_links(text)
+		expected_result = [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+
+		self.assertEqual(result, expected_result)
+
+
+	# text_node_to_html_node function
 	def test_text_node_to_html_node(self):
 		node = text_node_to_html_node(TextType.TEXT)
 		self.assertIsInstance(node, LeafNode)
--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from htmlnode import HTMLNode, LeafNode, ParentNode
+import re
 
 class TextType(Enum):
 	TEXT = "normal"
@@ -8,6 +9,7 @@ class TextType(Enum):
 	CODE = "code"
 	LINK = "link"
 	IMAGE = "image"
+
 
 class TextNode():
 	def __init__(self, text, text_type: TextType, url = None):
@@ -21,7 +23,172 @@ class TextNode():
 		return False
 
 	def __repr__(self):
-		return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+		if self.url:
+			return f"TextNode({self.text}, {self.text_type}, {self.url})"
+		return f"TextNode({self.text}, {self.text_type})"
+
+
+
+
+# Helper Methods
+def extract_markdown_text(text, delimiter):
+	matches = re.findall(fr"{re.escape(delimiter)}(.*?){re.escape(delimiter)}", text)
+	return matches
+
+def extract_markdown_images(text):
+	tuples_list = []
+	alt_text_matches = re.findall(r"\[(.*?)\]", text)
+	url_matches = re.findall(r"\((.*?)\)", text)
+
+	for i, alt_text in enumerate(alt_text_matches):
+		tuples_list.append((alt_text, url_matches[i]))
+
+	return tuples_list
+
+def extract_markdown_links(text):
+	tuples_list = []
+	anchor_text_matches = re.findall(r"\[(.*?)\]", text)
+	url_matches = re.findall(r"\((.*?)\)", text)
+
+	for i, anchor_text in enumerate(anchor_text_matches):
+		tuples_list.append((anchor_text, url_matches[i]))
+
+	return tuples_list
+
+
+
+
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+	all_nodes = []
+
+	for node in old_nodes:
+		nodes_list = []
+
+		if node.text_type != TextType.TEXT:
+			nodes_list.append(node)
+		else:
+			matches = extract_markdown_text(node.text, delimiter)
+
+			if matches:
+				pattern = fr"({re.escape(delimiter)}.*?{re.escape(delimiter)})"
+				sections = re.split(pattern, node.text)
+				matches_count = 0
+
+				for section in sections:
+					text = matches[matches_count]
+
+					if re.match(pattern, section):
+						nodes_list.append(TextNode(text = text, text_type = text_type))
+
+						if matches_count < len(matches)-1:
+							matches_count += 1
+					else:
+						nodes_list.append(TextNode(text = section, text_type = TextType.TEXT))
+			else:
+				nodes_list.append(node)
+
+		all_nodes.extend(nodes_list)
+
+	return all_nodes
+
+
+def split_nodes_images(old_nodes):
+	all_nodes = []
+
+	for node in old_nodes:
+		nodes_list = []
+
+		if node.text_type != TextType.TEXT:
+			nodes_list.append(node)
+
+		else:
+			tuples_list = extract_markdown_images(node.text)
+
+			if tuples_list:
+				pattern = r"(!\[.*?\]\(.*?\))"
+				sections = re.split(pattern, node.text)
+				tuple_count = 0
+
+				for section in sections:
+					alt_text = tuples_list[tuple_count][0]
+					url = tuples_list[tuple_count][1]
+
+					if re.match(pattern, section):
+						nodes_list.append(TextNode(text = alt_text, text_type = TextType.IMAGE, url = url))
+
+						if tuple_count < len(tuples_list)-1:
+							tuple_count += 1
+					elif section.strip():
+						nodes_list.append(TextNode(text = section, text_type = TextType.TEXT))
+			else:
+				nodes_list.append(node)
+
+		all_nodes.extend(nodes_list)
+
+	return all_nodes
+
+
+def split_nodes_links(old_nodes):
+	all_nodes = []
+
+	for node in old_nodes:
+		nodes_list = []
+
+		if node.text_type != TextType.TEXT:
+			nodes_list.append(node)
+
+		else:
+
+			tuples_list = extract_markdown_links(node.text)
+			pattern = r"(\[.*?\]\(.*?\))"
+			sections = re.split(pattern, node.text)
+
+			if tuples_list:
+				tuple_count = 0
+
+				for section in sections:
+					anchor_text = tuples_list[tuple_count][0]
+					url = tuples_list[tuple_count][1]
+
+					if re.match(pattern, section):
+						nodes_list.append(TextNode(text = anchor_text, text_type = TextType.LINK, url = url))
+
+						if tuple_count < len(tuples_list)-1:
+							tuple_count += 1
+					elif section.strip():
+						nodes_list.append(TextNode(text = section, text_type = TextType.TEXT))
+
+			else:
+				nodes_list.append(node)
+
+		all_nodes.extend(nodes_list)
+
+	return all_nodes
+
+
+def text_to_textnodes(text):
+	all_nodes = [TextNode(text = text, text_type = TextType.TEXT)]
+
+	# First split by Text Delimiters
+	delimiters = {
+		"**": TextType.BOLD, 
+		"*": TextType.ITALIC, 
+		"`": TextType.CODE}
+
+	for key, value in delimiters.items():
+		if split := split_nodes_delimiter(all_nodes, key, value):
+			all_nodes = split
+
+	# Finally split by Image and Link Delimiters
+	if split := split_nodes_images(all_nodes):
+		all_nodes = split
+	if split := split_nodes_links(all_nodes):
+		all_nodes = split
+
+	return all_nodes
+
 
 
 def text_node_to_html_node(text_node):
@@ -40,3 +207,11 @@ def text_node_to_html_node(text_node):
 			return LeafNode(tag = "img", value= " ", props = {"src": "url/image", "alt": "brief description"})
 		case _:
 			raise Exception("No type found")
+
+
+
+
+
+
+
+
